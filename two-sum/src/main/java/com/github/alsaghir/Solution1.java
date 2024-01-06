e9019ea23ee6f932e805d55cbf5116e6f77d650e
Saghir <ahmedalsaghirxyz@gmail.com>
2024-01-06 04:09:03 +0200

    rfq

--- two-sum/src/main/java/com/github/alsaghir/Solution1.java
+++ two-sum/src/main/java/com/github/alsaghir/Solution1.java
@@ -1,17 +1,77 @@
 package com.github.alsaghir;
 
+import java.util.Arrays;
+
 class Solution1 {
 
-    // O(n^2) runtime, O(1) space
-    // Brute force
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-        throw new IllegalStateException();
+    /* Searches an element key in a
+      pivoted sorted array arrp[]
+      of size n */
+    static int pivotedBinarySearch(int arr[], int n, int key)
+    {
+        int pivot = findPivot(arr, 0, n - 1);
+
+        // If we didn't find a pivot, then
+        // array is not rotated at all
+        if (pivot == -1)
+            return binarySearch(arr, 0, n - 1, key);
+
+        // If we found a pivot, then first
+        // compare with pivot and then
+        // search in two subarrays around pivot
+        if (arr[pivot] == key)
+            return pivot;
+        if (key >= arr[0])
+            return binarySearch(arr, 0, pivot - 1, key);
+        return binarySearch(arr, pivot + 1, n - 1, key);
+    }
+
+    /* Function to get pivot. For array
+       3, 4, 5, 6, 1, 2 it returns
+       3 (index of 6) */
+    static int findPivot(int arr[], int low, int high)
+    {
+        // base cases
+        if (high < low)
+            return -1;
+        if (high == low)
+            return low;
+
+        /* low + (high - low)/2; */
+        int mid = (low + high) / 2;
+        if (mid < high && arr[mid] > arr[mid + 1])
+            return mid;
+        if (mid > low && arr[mid] < arr[mid - 1])
+            return (mid - 1);
+        if (arr[low] >= arr[mid])
+            return findPivot(arr, low, mid - 1);
+        return findPivot(arr, mid + 1, high);
+    }
+
+    /* Standard Binary Search function */
+    static int binarySearch(int arr[], int low, int high, int key)
+    {
+        if (high < low)
+            return -1;
+
+        Arrays.sort(new int[]{1, 2});
+        /* low + (high - low)/2; */
+        int mid = (low + high) / 2;
+        if (key == arr[mid])
+            return mid;
+        if (key > arr[mid])
+            return binarySearch(arr, (mid + 1), high, key);
+        return binarySearch(arr, low, (mid - 1), key);
+    }
+
+    // main function
+    public static void main(String args[])
+    {
+        // Let us search 3 in below array
+        int arr1[] = { 5, 6, 7, 8, 9, 10, 1, 2, 3 };
+        int n = arr1.length;
+        int key = 3;
+        System.out.println("Index of the element is : "
+                + pivotedBinarySearch(arr1, n, key));
     }
 }
